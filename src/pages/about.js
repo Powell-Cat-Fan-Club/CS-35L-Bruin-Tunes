005b387963835f1bb7e9cd5b67ff3cdc6d70a657
Laura Ness <93940826+lness02@users.noreply.github.com>
2022-04-29 15:18:07 -0700

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+//credit to https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
+import React from "react";
+  
+const About = () => {
+  return (
+    <div>
+      <h1>
+        BruinTunes is tunes for bruins. 
+      </h1>
+    </div>
+  );
+};
+  
+export default About;
---
+++ src/pages/comments.js
@@ -0,0 +1,12 @@
+//credit to https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
+
+import React from 'react'; 
+const Comments = () => {
+  return (
+    <div>
+      <h1>Comments</h1>
+    </div>
+  );
+};
+  
+export default Comments;
