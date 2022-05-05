124a699dfac598e7a2fbfdc16dcbc3488cb8ad43
Daniel Zhu <danielzhu21347@gmail.com>
2022-05-04 18:17:29 -0700

    Created basic ranking system

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,9 +1,23 @@
 //credit to https://www.geeksforgeeks.org/how-to-create-a-multi-page-website-using-react-js/
 import React from 'react';
+import { getArtists } from "./artist-data";
 const List = () => {
+
+  let artists = getArtists();
+  let toSort = [].concat(artists);
+  toSort.sort((a,b) => a.ranking - b.ranking);
+  
+  let displayRanking = () => {
+    return (
+    <div>
+      {toSort.map((artist) => <li>{artist.name + " Likes:" + artist.ranking}</li>)}
+    </div>
+    )
+  }
   return (
     <div>
-      <h1>Rank the albums</h1>
+      <h1>Albums as ranked by UCLA students</h1>
+      {displayRanking()}
     </div>
   );
 };
