f7adb833ec0791ee50a4f1600fba4c55d9a79f28
Tiffany Hwang <tiffh2003@gmail.com>
2022-05-10 15:31:32 -0700

    Added padding to artist, album, and song pages, made the video player change size according to window width, centered Navbar elements

--- src/pages/songs.js
+++ src/pages/songs.js
@@ -9,14 +9,40 @@ import { useParams, NavLink, Outlet } from "react-router-dom";
 import { getSong } from './artist-data';
 import Youtube from "react-youtube";
 
+import { useState, useEffect } from 'react';
   
 
 const Songs = () => {
     let params = useParams();
     let song = getSong(params.artistID, params.albumID, params.songID);
 
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+        return {
+          width,
+          height
+        };
+      }
+      
+      function useWindowDimensions() {
+        const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+      
+        useEffect(() => {
+          function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+          }
+      
+          window.addEventListener('resize', handleResize);
+          return () => window.removeEventListener('resize', handleResize);
+        }, []);
+      
+        return windowDimensions;
+      }
+      
+
     // Render function for Prismic headless CMS pages
     function Content() {
+        const { width } = useWindowDimensions();
         let videoUrl = song.url;
         let videoCode;
         if (videoUrl) {
@@ -25,6 +51,8 @@ const Songs = () => {
     
     
         const opts = {
+            height:  width / 3 * 9 / 16,
+            width: width / 3,
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             autoplay: 1
@@ -48,13 +76,17 @@ const Songs = () => {
         <div style={{ display: "flex" }}>
           <nav
           style={{
-            justifyContent: "center",
+            padding: "2rem"
           }}
         >
           <h2>Song name: {song.name}</h2>
           {Content()}
         </nav>
-        <NavLink to={`/artists/${params.artistID}/${params.albumID}`}> Back </NavLink>
+        <nav style = {{
+            padding:"2rem"
+        }}>
+            <NavLink to={`/artists/${params.artistID}/${params.albumID}`}> Back </NavLink>
+        </nav>
         </div>
     )
 
