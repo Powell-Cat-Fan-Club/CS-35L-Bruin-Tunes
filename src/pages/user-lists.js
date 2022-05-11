b8db742dfe997497544830d181259580aaa08fd4
Daniel Zhu <danielzhu21347@ucla.edu>
2022-05-11 16:54:44 -0700

    Renamed the list/rankings tab to Charts and added new tab for personalized user lists. Added ability to search by genre on charts

---
+++ src/pages/user-lists.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getArtists } from "./artist-data";
+import { useState } from 'react';
+import { Link} from "react-router-dom";
+
+
+const UserList = () => {
+    return (
+        <div>
+            <h1>Create your own personalized list!</h1>
+        </div>
+    )
+}
+
+export default UserList;
